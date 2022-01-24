86199a7bc844098247a6d4b7dd121213d8204f06
develop-heo <44943368+develop-heo@users.noreply.github.com>
2022-01-24 23:56:40 +0900

    [ADD] Move to menu

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+//Move to section
+// 메뉴를 클릭하면 클릭한 요소의 top 위치를 가져와서 scrollTo로 해당 위치로 이동
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+  const target = e.target.dataset.link;
+  if (target == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(target);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
